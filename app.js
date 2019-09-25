53ec328dd6d23f9816242f0a05a35b9e83a4368d
dimabur <dimlelik@rambler.ru>
2019-09-25 22:32:33 +0300

    fixes #5

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 console.log('worked');
+
+const regularPurchases = 10000;
+const increasedPurchases = 50000;
+const specialPurchases = 80000;
+
 const regularPercent = 0.01;
 const increasedPercent = 0.05;
 const specialPercent = 0.3;
 
-const previousPurchases = 16000; // сумма конкретной покупки
+let cashback = regularPurchases * regularPercent + increasedPurchases * increasedPercent + specialPurchases * specialPercent;
 
-let cashback;
-if (previousPurchases <= 15000) {
-    cashback = previousPurchases * regularPercent;
-} else if (previousPurchases <= 150000) {
-    cashback = previousPurchases * increasedPercent;
-} else {
-    cashback = 3000;
+let maxCashback = 3000;
+
+if (cashback > 15000) {
+    cashback = maxCashback;
 }
 console.log(cashback + ' ' + 'рублей');
